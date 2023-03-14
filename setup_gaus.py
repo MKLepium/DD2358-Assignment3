430c73f44841dafdd3e846402781056067384c55
Maximilian Georg Kurzawski <maximilian-georg.kurzawski@codecentric.de>
2023-03-14 17:23:20 +0100

    yeet

--- setup_gaus.py
+++ setup_gaus.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
+import Cython.Compiler.Options
+Cython.Compiler.Options.annotate = True
+
 import numpy
-setup(ext_modules=cythonize("gauss_seidel_cython.pyx", compiler_directives={"language_level": "3"}), 
+setup(ext_modules=cythonize("gauss_seidel_cython.pyx", compiler_directives={"language_level": "3"}, annotate=True), 
                             include_dirs=[numpy.get_include()])
